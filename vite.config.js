bd86e2558503b33185e75595d62191e67b4c25c2
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-05-18 10:52:42 -0400

    Configure for custom domain name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/a-to-z-coaching",
+  base: "/",
   plugins: [react(), tailwindcss(),],
 })
